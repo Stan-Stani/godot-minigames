9a9e9cd9d16d18f4a911a792e66f4848cf15aecf
biometricPsychography <cyclinglinguist@gmail.com>
2025-09-04 18:20:02 -0500

    feat: being basic health system

--- glasun.gd
+++ glasun.gd
@@ -1,11 +1,23 @@
-extends StaticBody2D
+extends CharacterBody2D
+var move: bool = true
 
 @onready var colleen: CharacterBody2D = %Colleen
 
 func _physics_process(delta: float) -> void:
-	if colleen.position.x > self.position.x:
-		print('go right')
-		self.position.x += delta * 5
-	elif colleen.position.x < self.position.y:
-		print('go left')
-		self.position.x -= delta * 5
+	if move:
+		if round(colleen.position.x) > round(self.position.x):
+			self.position.x += delta * 5
+		elif round(colleen.position.x) < round(self.position.y):
+			self.position.x -= delta * 5
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == %Colleen:
+		EventBus.player_health_change.emit(-1)
+		move = false
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	print('exit')
+	if body == %Colleen:
+		move = true
--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+
 @export var speed_base = 100
 @export var gravity = 200
 @export var jump_force_base = 150
@@ -10,13 +11,16 @@ var jump_force = jump_force_base
 var was_just_in_air: bool = true
 
 
+func _ready() -> void:
+	EventBus.player_health_change.connect(_on_player_health_change)
+
 func _process(_delta: float) -> void:
 	if self.is_on_floor():
 		if (Input.is_action_pressed("ui_right")
 			|| Input.is_action_pressed("ui_left")):
-				$Colleen.play('walk')
-		elif !was_just_in_air && $Colleen.animation != 'jump':
-			$Colleen.play('idle')
+				$AnimatedSprite2D.play('walk')
+		elif !was_just_in_air && $AnimatedSprite2D.animation != 'jump':
+			$AnimatedSprite2D.play('idle')
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
@@ -35,9 +39,9 @@ func handle_horizontal_input():
 	velocity.x = horizontal_input * speed
 
 	if horizontal_input > 0:
-		$Colleen.scale.x = -1
+		$AnimatedSprite2D.scale.x = -1
 	elif horizontal_input < 0:
-		$Colleen.scale.x = 1
+		$AnimatedSprite2D.scale.x = 1
 
 func handle_jump_input():
 	var jump_input: float = - (
@@ -49,13 +53,13 @@ func handle_jump_input():
 		velocity.y = jump_input * jump_force
 		if jump_input != 0:
 			was_just_in_air = true
-			$Colleen.play('jump', 2)
+			$AnimatedSprite2D.play('jump', 2)
 
 func handle_land():
 	if !self.is_on_floor():
 		was_just_in_air = true
 	if was_just_in_air && self.is_on_floor():
-		$Colleen.play('jump')
+		$AnimatedSprite2D.play('jump')
 		was_just_in_air = false
 
 func handle_distance_from_wand():
@@ -63,4 +67,7 @@ func handle_distance_from_wand():
 	if distance >= 100:
 		jump_force = jump_force_base + jump_force_base * (distance / 1000)
 		speed = speed_base + speed_base * (distance / 1000)
-	print(distance, jump_force_base, speed_base)
+	# print(distance, jump_force_base, speed_base)
+
+func _on_player_health_change(delta: int):
+	print("delta health ", delta)
